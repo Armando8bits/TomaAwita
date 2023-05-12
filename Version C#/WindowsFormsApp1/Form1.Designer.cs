cd33a8739c17216fa83fe16784386dfe8ed64096
Roque A Ramírez <109245164+Armando8bits@users.noreply.github.com>
2023-05-12 15:38:34 -0500

    Add files via upload
    Agregada versión en C#

---
+++ Version C#/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.txtKG = new System.Windows.Forms.TextBox();
+            this.txtVaso = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Su Peso en KG:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MiliLitro por Vaso:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(73, 171);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(150, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Creado por: Armando Ramírez";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            // 
+            // txtKG
+            // 
+            this.txtKG.Location = new System.Drawing.Point(109, 6);
+            this.txtKG.Name = "txtKG";
+            this.txtKG.Size = new System.Drawing.Size(62, 20);
+            this.txtKG.TabIndex = 3;
+            this.txtKG.Text = "85";
+            this.txtKG.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtVaso
+            // 
+            this.txtVaso.Location = new System.Drawing.Point(109, 32);
+            this.txtVaso.Name = "txtVaso";
+            this.txtVaso.Size = new System.Drawing.Size(62, 20);
+            this.txtVaso.TabIndex = 4;
+            this.txtVaso.Text = "500";
+            this.txtVaso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(120, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 195);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtVaso);
+            this.Controls.Add(this.txtKG);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TomaAwita! uwu V4.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox txtKG;
+        private System.Windows.Forms.TextBox txtVaso;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Version C#/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool ProgramaActivo = false;
+        float FLitros = 0;
+        float FPeso = 0;
+        float FMililitro = 0;
+        int Horas = 0, Minutos = 0, Segundos = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float jj; /*La variable JJ la hice para no hacer if (FLitros - FMililitro >= 0) ya que eso me estaba retando la variable
+                  causando fallos logicos en el programa*/
+            Segundos++;
+            if (Segundos == 60)
+            {
+                Segundos = 0;
+                Minutos++;
+                if (Minutos == 60)
+                {
+                    Minutos = 0;
+                    Horas++;
+                    jj = FLitros - FMililitro;
+                    if (jj >= 0)
+                    {
+                        FLitros -= FMililitro;
+                    }
+                    else
+                    {
+                        FLitros = 0;
+                    }
+                    if (Horas == 24)
+                    {
+                        Horas = 0;
+                        MessageBox.Show("Trabajar 24 Horas Seguidas es inhumano, si no vas a descansar, yo me la saco, Bye!.", "Ya Descansa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        this.Close(); // cierra el programa al trabajar 24 horas seguidas
+                    }
+                    this.WindowState = FormWindowState.Maximized;// maximiza la ventana
+                    MessageBox.Show("Intenta mirar hacia arriba. Siéntate derecho y lleva la mirada hacia algo lejano, despues al extremo inferior derecho y de ahí hacia el cielo, luego a la izquierda y de vuelta hacia abajo. Ahora mueve los ojos en la dirección contraria. Repite cinco veces.", "Notificacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    label4.Text = "MiliLitros por tomar : " + FLitros;
+                    this.WindowState = FormWindowState.Minimized;// minimiza la ventana
+                }
+                else if (Minutos == 30)
+                {
+                    this.WindowState = FormWindowState.Maximized;// maximiza la ventana
+                    MessageBox.Show("Levantate, camina, estirate, dobla el tronco (como saludo japonés e izquierda-derecha.)", "Notificacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.WindowState = FormWindowState.Minimized;// minimiza la ventana
+                }
+            }
+            label5.Text = Horas.ToString("00") +":" + Minutos.ToString("00") + ":"+Segundos.ToString("00");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ProgramaActivo == true)
+            {
+                DialogResult rpta = MessageBox.Show("Desea Dejar de recibir el recordatorio? No te olvides de seguir tomando Aguita uwu", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if(rpta == DialogResult.No)
+                {
+                    e.Cancel = true; //cancela el cierre
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FPeso = float.Parse(txtKG.Text);
+            FMililitro = float.Parse(txtVaso.Text);
+            if(FPeso > 1 && FMililitro > 1)
+            {
+                FLitros = FPeso * 35; // Mililitros de Agua a tomar en un día (35ml x cada kilo)
+                DialogResult rpta = MessageBox.Show("Todo calculo del agua a tomar se hace en base al peso introducido", "Aclaración", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (rpta == DialogResult.Yes)
+                {
+                    ProgramaActivo = true;
+                    button1.Enabled = false;
+                    txtKG.Enabled = false;
+                    txtVaso.Enabled = false;
+                    label3.Text = "MiliLitros Sugeridos: " + FLitros;
+                    this.WindowState= FormWindowState.Minimized;// minimiza la ventana
+                    timer1.Enabled = true;
+                }
+            }
+
+        }
+    }
+}
